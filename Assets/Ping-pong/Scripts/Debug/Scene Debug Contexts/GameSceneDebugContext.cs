4d97a0d26f3f5535f7a00e829233876c5954a106
Joseph <ziyoyevyusuf@gmail.com>
2024-12-10 13:09:11 +0500

    Added Debug scripts. Added Debug game scene context. Added Reset button to Default Inputs

---
+++ Assets/Ping-pong/Scripts/Debug/Scene Debug Contexts/GameSceneDebugContext.cs
@@ -0,0 +1,42 @@
+﻿using Runtime.Basics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Debug.SceneContexts
+{
+    public class GameSceneDebugContext : MonoBehaviour
+    {
+        #region INPUT_ACTIONS
+
+        private InputAction _resetButton;
+        
+        #endregion
+
+        private RoundRelatedBehaviour[] _roundRelatedObjects;
+        
+        private void Awake()
+        {
+            _resetButton = InputSystem.actions.FindAction("Reset");
+            
+            _roundRelatedObjects = FindObjectsByType<RoundRelatedBehaviour>(FindObjectsSortMode.None);
+        }
+
+        private void OnEnable()
+        {
+            _resetButton.started += ResetScene;
+        }
+
+        private void OnDisable()
+        {
+            _resetButton.started -= ResetScene;
+        }
+
+        private void ResetScene(InputAction.CallbackContext callbackContext)
+        {
+            foreach (RoundRelatedBehaviour obj in _roundRelatedObjects)
+            {
+                obj.Reset();
+            }
+        }
+    }
+}
