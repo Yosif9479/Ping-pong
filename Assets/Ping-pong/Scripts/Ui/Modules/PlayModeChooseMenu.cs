c7c02f689ba4322587beff170ad719b4afbfbbc4
Joseph <ziyoyevyusuf@gmail.com>
2024-12-10 18:06:14 +0500

    Added main menu with build profile

---
+++ Assets/Ping-pong/Scripts/Ui/Modules/PlayModeChooseMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ui.Modules
+{
+    public class PlayModeChooseMenu : MonoBehaviour
+    {
+        public void ChooseMultiplayer()
+        {
+            SceneManager.LoadScene("Game");   
+        }
+
+        public void ChooseSolo()
+        {
+            UnityEngine.Debug.LogError("Solo game doesn't implemented yet");
+        }
+    }
+}
